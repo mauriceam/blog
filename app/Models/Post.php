6bc3d0654eea85b2c4034f9a2a3c8b6efe3fc1ff
mauriceam <ammann.maurice@gmail.com>
2023-02-09 15:00:14 +0100

    Second commit

---
+++ app/Models/Post.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post
+{
+    public $title;
+    public $excerpt;
+    public $date;
+    public $slug;
+    public $body;
+
+    public function __construct($title, $excerpt, $date, $slug, $body)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->slug = $slug;
+        $this->body = $body;
+    }
+
+    public static function find($slug)
+    {
+        return static::all()->firstWhere('slug', $slug);
+    }
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
+
+    }
+
+    public static function all()
+    {
+        return cache()->rememberForever('posts.all',function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->slug,
+                    $document->body()
+                ))
+                ->sortByDesc('date');
+        });
+
+    }
+}
---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <article>
+        <h1>
+            {{$post->title}}
+        </h1>
+
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+
+    <a href="/">Go Back Home</a>
+</x-layout>
+
+
